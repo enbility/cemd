30263c8ed3c5135c578d180ffc7e7545baf5b071
Andreas Linde <mail@andreaslinde.de>
2022-12-14 22:13:29 +0100

    Add interface methods

--- grid/grid.go
+++ grid/grid.go
@@ -8,6 +8,13 @@ import (
 )
 
 type GridI interface {
+	PowerLimitationFactor() (float64, error)
+	MomentaryPowerConsumptionOrProduction() (float64, error)
+	TotalFeedInEnergy() (float64, error)
+	TotalConsumedEnergy() (float64, error)
+	MomentaryCurrentConsumptionOrProduction() ([]float64, error)
+	Voltage() ([]float64, error)
+	Frequency() (float64, error)
 }
 
 type GridImpl struct {
