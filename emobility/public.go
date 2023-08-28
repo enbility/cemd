20651f260c714da6cbf384a204d854e555feada3
Andreas Linde <mail@andreaslinde.de>
2023-08-28 13:08:51 +0200

    Fix EVLoadControlObligationLimits
    If the limit is inactive, report the maximum value

--- emobility/public.go
+++ emobility/public.go
@@ -317,8 +317,8 @@ func (e *EMobilityImpl) EVLoadControlObligationLimits() ([]float64, error) {
 		}
 
 		var limitValue float64
-		if limitIdData.Value == nil {
-			// assume maximum possible
+		if limitIdData.Value == nil || (limitIdData.IsLimitActive != nil && *limitIdData.IsLimitActive == false) {
+			// report maximum possible if no limit is available or the limit is not active
 			_, dataMax, _, err := e.evElectricalConnection.GetLimitsForParameterId(*elParamDesc.ParameterId)
 			if err != nil {
 				return nil, features.ErrDataNotAvailable
