24344bec84a0476a4402bb0201476f58c07a82d7
Andreas Linde <mail@andreaslinde.de>
2024-04-05 13:18:22 +0200

    Minor cleanup

--- cmd/democem/service.go
+++ cmd/democem/service.go
@@ -6,9 +6,6 @@ import (
 )
 
 // report the Ship ID of a newly trusted connection
-func (d *DemoCem) RemoteServiceShipIDReported(service eebusapi.ServiceInterface, ski string, shipID string) {
-}
-
 func (d *DemoCem) RemoteSKIConnected(service eebusapi.ServiceInterface, ski string) {}
 
 func (d *DemoCem) RemoteSKIDisconnected(service eebusapi.ServiceInterface, ski string) {}
@@ -19,5 +16,3 @@ func (d *DemoCem) VisibleRemoteServicesUpdated(service eebusapi.ServiceInterface
 func (h *DemoCem) ServiceShipIDUpdate(ski string, shipdID string) {}
 
 func (h *DemoCem) ServicePairingDetailUpdate(ski string, detail *shipapi.ConnectionStateDetail) {}
-
-func (h *DemoCem) AllowWaitingForTrust(ski string) bool { return true }
