5f68512873eec0941e9d363e31853e67d2a6c3c9
Andreas Linde <mail@andreaslinde.de>
2024-04-09 14:11:26 +0200

    Add LPC Server to demo cmd

--- cmd/democem/democem.go
+++ cmd/democem/democem.go
@@ -3,6 +3,7 @@ package democem
 import (
 	"github.com/enbility/cemd/cem"
 	"github.com/enbility/cemd/ucevsecc"
+	"github.com/enbility/cemd/uclpcserver"
 	eebusapi "github.com/enbility/eebus-go/api"
 	"github.com/enbility/ship-go/logging"
 )
@@ -24,6 +25,9 @@ func (d *DemoCem) Setup() error {
 	if err := d.cem.Setup(); err != nil {
 		return err
 	}
+
+	lpcs := uclpcserver.NewUCLPC(d.cem.Service, d.entityEventCB)
+	d.cem.AddUseCase(lpcs)
 
 	evsecc := ucevsecc.NewUCEVSECC(d.cem.Service, d.entityEventCB)
 	d.cem.AddUseCase(evsecc)
